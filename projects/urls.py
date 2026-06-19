5c08c596211fe85dd55d1e012500d76f61f54a78
Vasilisk16 <155679330+Vasilisk16@users.noreply.github.com>
2026-06-19 19:04:59 +0300

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("list", views.project_list_view),
-    path("list/", views.project_list_view),
+    path("list/", views.project_list_view, name="project_list"),
     path("create-project/", views.project_create_view),
     path("skills/", views.skill_autocomplete_view),
-    path("<int:project_id>/", views.project_detail_view),
+    path("<int:project_id>/", views.project_detail_view, name="project_detail"),
     path("<int:project_id>/edit", views.project_edit_view),
     path("<int:project_id>/complete/", views.project_complete_view),
     path("<int:project_id>/toggle-participate/", views.toggle_participate_view),
