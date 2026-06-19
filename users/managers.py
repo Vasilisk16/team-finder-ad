f04b178d64bc71f637827cdc9875e71d38c509d3
Vasilisk16 <155679330+Vasilisk16@users.noreply.github.com>
2026-06-19 19:03:35 +0300

    Add files via upload

---
+++ users/managers.py
@@ -0,0 +1,26 @@
+import uuid
+
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обязателен")
+        email = self.normalize_email(email)
+        if not extra_fields.get("phone"):
+            extra_fields["phone"] = f"+7900{uuid.uuid4().hex[:7]}"
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("name", "Admin")
+        extra_fields.setdefault("surname", "User")
+        if not extra_fields.get("phone"):
+            extra_fields["phone"] = f"+7900{uuid.uuid4().hex[:7]}"
+        return self.create_user(email, password, **extra_fields)
